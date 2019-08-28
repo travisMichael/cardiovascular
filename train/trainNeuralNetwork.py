49aad5d27eb8082f479e22d9a41e4bffc84e1333
Latzke, Travis-CW <Travis.Latzke@bestbuy.com>
2019-08-28 16:40:48 -0500

    Successful neural network implementation

--- train/trainNeuralNetwork.py
+++ train/trainNeuralNetwork.py
@@ -1,22 +1,41 @@
+# Resources
 # https://scikit-learn.org/stable/modules/neural_networks_supervised.html
+# https://scikit-learn.org/stable/auto_examples/neural_networks/plot_mlp_training_curves.html
 from sklearn.neural_network import MLPClassifier
+from sklearn.linear_model import LogisticRegression
+import numpy as np
+import pickle
+
+N_CLASSES = np.unique([0 , 1])
 
 X = [[0., 0.], [1., 1.], [-1., 1.], [0., 0.]]
 y = [0, 1, 1, 1]
-clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(120, 2), random_state=1, max_iter=1)
+model = MLPClassifier(solver='sgd', validation_fraction=0.15, alpha=1e-4, hidden_layer_sizes=(10, 10), random_state=1)
 
+x_train_file = open('../data/train/x', 'rb')
+y_train_file = open('../data/train/y', 'rb')
+x_test_file = open('../data/test/x', 'rb')
+y_test_file = open('../data/test/y', 'rb')
+x_train = pickle.load(x_train_file)
+y_train = pickle.load(y_train_file)
+x_test = pickle.load(x_test_file)
+y_test = pickle.load(y_test_file)
 
-clf.fit(X, y)
+model.fit(x_train, y_train)
+# model.partial_fit(x_train, y_train, classes=N_CLASSES)
+result = model.predict(x_test)
 
-prob_result = clf.predict_proba([[2., 2.], [1., 2.]])
-print(prob_result)
+correct = 0
+incorrect = 0
+for i in range(len(y_test)):
+    if y_test[i] == result[i]:
+        correct += 1
+    else:
+        incorrect += 1
+print(correct / (correct + incorrect))
 
-clf.fit(X, y)
-clf.fit(X, y)
-clf.fit(X, y)
-
-# result = clf.predict([[2., 2.], [-1., -2.]])
-# print(result)
-
-prob_result = clf.predict_proba([[2., 2.], [1., 2.]])
-print(prob_result)
+x_train_file.close()
+y_train_file.close()
+x_test_file.close()
+y_test_file.close()
+print("done")
