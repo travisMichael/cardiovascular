f7f073bb1cd83fad0354b6224269198c19f23337
Latzke, Travis-CW <Travis.Latzke@bestbuy.com>
2019-09-04 15:09:47 -0500

    Added timing to plot function

--- visualization_utils.py
+++ visualization_utils.py
@@ -2,6 +2,7 @@ from sklearn.model_selection import learning_curve
 from sklearn.model_selection import ShuffleSplit
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 # The learning_curve_plot function is an adapted version from:
 # https://scikit-learn.org/stable/auto_examples/model_selection/plot_learning_curve.html
@@ -13,6 +14,8 @@ def multiple_learning_curves_plot(model_list, x, y, colors, training_labels):
     plt.figure()
 
     for i in range(number_of_models):
+        print("Generating learning curve for model: " + str(i))
+        start_time = time.time()
         model = model_list[i]
         curve_color = colors[i]
         label = training_labels[i]
@@ -28,5 +31,6 @@ def multiple_learning_curves_plot(model_list, x, y, colors, training_labels):
         plt.fill_between(train_sizes, test_scores_mean - test_scores_std, test_scores_mean + test_scores_std, alpha=0.1, color=curve_color)
         plt.plot(train_sizes, train_scores_mean, 'o-', color=curve_color, label=label)
         plt.plot(train_sizes, test_scores_mean, 'o-', color=curve_color, linestyle='dashed')
-
+        end_time = time.time() - start_time
+        print("Learning curve finished for model: " + str(i) + " " + str(end_time))
     return plt
