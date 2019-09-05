b8a7008b90d7fb264af60737040336d59b30e0b7
Latzke, Travis-CW <Travis.Latzke@bestbuy.com>
2019-09-05 17:16:45 -0500

    Customized train.py with command line arguments

--- train.py
+++ train.py
@@ -8,25 +8,24 @@ from train.trainNeuralNetwork import train_neural_net
 if __name__ == "__main__":
     print(len(sys.argv))
     if len(sys.argv) < 3:
-        print("Please specify filename, dataset, and model")
+        print("Please specify filename, data set, and model")
     else:
-        dataset = sys.argv[1]
+        data_set = sys.argv[1]
         model = sys.argv[2]
         if model == 'kNN':
-            train_k_NN(dataset, '')
+            train_k_NN(data_set, '')
         if model == 'Boosted':
-            train_boosted_dtc(dataset, '')
+            train_boosted_dtc(data_set, '')
         if model == 'DTC':
-            train_dtc(dataset, '')
+            train_dtc(data_set, '')
         if model == 'NeuralNet':
-            train_neural_net(dataset, '')
+            train_neural_net(data_set, '')
         if model == 'svm':
-            train_svm(dataset, '')
+            train_svm(data_set, '')
         if model == 'all':
-            train_k_NN(dataset, '')
-            train_boosted_dtc(dataset, '')
-            train_dtc(dataset, '')
-            train_neural_net(dataset, '')
-            train_svm(dataset, '')
+            train_k_NN(data_set, '')
+            train_boosted_dtc(data_set, '')
+            train_dtc(data_set, '')
+            train_neural_net(data_set, '')
+            train_svm(data_set, '')
 
-        print("done")
