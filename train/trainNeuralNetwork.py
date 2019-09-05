6ad862cea3f4287a977acfac31d3be02c78052a6
Latzke, Travis-CW <Travis.Latzke@bestbuy.com>
2019-09-05 10:28:39 -0500

    Made trainNeuralNetwork runnable from terminal

--- train/trainNeuralNetwork.py
+++ train/trainNeuralNetwork.py
@@ -4,51 +4,27 @@
 from sklearn.neural_network import MLPClassifier
 import numpy as np
 import pickle
-import os
+from utils import save_model
 
-N_CLASSES = np.unique([0 , 1])
 
-model = MLPClassifier(solver='sgd', validation_fraction=0.0, alpha=1e-4, hidden_layer_sizes=(10, 10), random_state=1)
+def train_neural_net(dataset, path):
+    print('Training Neural Network...')
+    model = MLPClassifier(solver='sgd', validation_fraction=0.0, alpha=1e-4, hidden_layer_sizes=(10, 10), random_state=1)
 
-x_train_file = open('../data/train/x', 'rb')
-y_train_file = open('../data/train/y', 'rb')
-x_test_file = open('../data/test/x', 'rb')
-y_test_file = open('../data/test/y', 'rb')
-x_train = pickle.load(x_train_file)
-y_train = pickle.load(y_train_file)
-x_test = pickle.load(x_test_file)
-y_test = pickle.load(y_test_file)
+    x_train_file = open(path + 'data/train/x', 'rb')
+    y_train_file = open(path + 'data/train/y', 'rb')
+    x_train = pickle.load(x_train_file)
+    y_train = pickle.load(y_train_file)
 
-model.fit(x_train, y_train)
-# model.partial_fit(x_train, y_train, classes=N_CLASSES)
+    model.fit(x_train, y_train)
 
-result = model.predict(x_train)
-correct = 0
-incorrect = 0
-for i in range(len(y_train)):
-    if y_train[i] == result[i]:
-        correct += 1
-    else:
-        incorrect += 1
-print(correct / (correct + incorrect))
+    save_model(model, dataset, 'best_neural_net_model')
 
-result = model.predict(x_test)
-correct = 0
-incorrect = 0
-for i in range(len(y_test)):
-    if y_test[i] == result[i]:
-        correct += 1
-    else:
-        incorrect += 1
-print(correct / (correct + incorrect))
+    x_train_file.close()
+    y_train_file.close()
+    print("done")
 
-if not os.path.exists('../model'):
-    os.makedirs('../model')
 
-pickle.dump(model, open("../model/best_Neural_Network_model", 'wb'))
-
-x_train_file.close()
-y_train_file.close()
-x_test_file.close()
-y_test_file.close()
-print("done")
+if __name__ == "__main__":
+    print('hello')
+    train_neural_net('cardio', '../')
