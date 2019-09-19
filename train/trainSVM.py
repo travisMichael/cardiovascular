91964975c2c76cd77a825607d40b277d620b1dc1
Latzke, Travis-CW <Travis.Latzke@bestbuy.com>
2019-09-18 21:44:19 -0500

    Updates to trainSVM

--- train/trainSVM.py
+++ train/trainSVM.py
@@ -9,8 +9,8 @@ def train_svm(path, with_plots):
 
     if not with_plots:
         print('Training SVM...')
-        model_1 = train_and_time(svm.SVC(kernel='linear', cache_size=400), x_train, y_train)
-        model_2 = train_and_time(svm.SVC(kernel='poly', cache_size=400), x_train, y_train)
+        model_1 = train_and_time(svm.SVC(kernel='linear', cache_size=400, probability=True), x_train, y_train)
+        model_2 = train_and_time(svm.SVC(kernel='poly', cache_size=400, probability=True), x_train, y_train)
 
         save_model(model_1, path + 'model/' + data_set, 'svm_model_1')
         save_model(model_2, path + 'model/' + data_set, 'svm_model_2')
@@ -24,11 +24,11 @@ def train_svm_loan(path, with_plots):
 
     if not with_plots:
         print("Training SVM...")
-        model_1 = train_and_time(svm.SVC(kernel='linear', cache_size=400), x_train, y_train)
-        # model_2 = train_and_time(svm.SVC(kernel='poly', cache_size=400), x_train, y_train)
+        model_1 = train_and_time(svm.SVC(kernel='linear', cache_size=400, probability=True), x_train, y_train)
+        model_2 = train_and_time(svm.SVC(kernel='poly', cache_size=400, probability=True), x_train, y_train)
 
         save_model(model_1, path + 'model/' + data_set, 'svm_model_1')
-        # save_model(model_2, path + 'model/' + data_set, 'svm_model_2')
+        save_model(model_2, path + 'model/' + data_set, 'svm_model_2')
 
     else:
         print('No learning curves for SVM...')
